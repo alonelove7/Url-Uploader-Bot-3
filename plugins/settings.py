e8ef632d958013b8b94009ffb68c0aef8c33f565
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 12:04:36 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -1,1 +1,26 @@
+# (c) @Tellybots
 
+import asyncio
+from plugins.database.add import db
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def OpenSettings(event: Message, user_id: int):
+    try:
+        await event.edit(
+            text="**⚙ Configure My Behaviour**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(f"🔰 Upload as {'File 🗃️' if ((await db.get_upload_as_doc(user_id)) is True) else 'Video 🎥'}",
+                                          callback_data="triggerUploadMode")],
+                    [InlineKeyboardButton("🌆 Custom Thumbnail ", callback_data="triggerThumbnail")],
+                    [InlineKeyboardButton("⛔ Close Settings", callback_data="closeMeh")]
+                ]
+            )
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await OpenSettings(event, user_id)
+    except MessageNotModified:
+        pass
