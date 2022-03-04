9d42d20e0d94cee66b68d9be24a8e569da4950fa
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 14:09:58 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -299,3 +299,30 @@ async def youtube_dl_call_back(bot, update):
                 message_id=update.message.message_id,
                 disable_web_page_preview=True
             )
+
+@Client.on_callback_query()
+async def button(bot, update):
+    if "|" in update.data:
+        await youtube_dl_call_back(bot, update)
+    elif "=" in update.data:
+        await ddl_call_back(bot, update)
+    elif update.data == "home":
+        await update.message.edit_text(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=Translation.HELP_TEXT,
+            reply_markup=Translation.HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=Translation.ABOUT_TEXT,
+            reply_markup=Translation.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
