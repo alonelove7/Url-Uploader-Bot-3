6aec494e6bb0d02dac7d34476db16bafffe25ed9
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 14:08:52 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -16,32 +16,7 @@ from plugins.link import ddl_call_back
 from plugins.main import Translation
 from plugins.database.database import db
 
-@Client.on_callback_query()
-async def button(bot, update):
-    if "|" in update.data:
-        await youtube_dl_call_back(bot, update)
-    elif "=" in update.data:
-        await ddl_call_back(bot, update)
-    elif update.data == "home":
-        await update.message.edit_text(
-            text=Translation.START_TEXT.format(update.from_user.mention),
-            reply_markup=Translation.START_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "help":
-        await update.message.edit_text(
-            text=Translation.HELP_TEXT,
-            reply_markup=Translation.HELP_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "about":
-        await update.message.edit_text(
-            text=Translation.ABOUT_TEXT,
-            reply_markup=Translation.ABOUT_BUTTONS,
-            disable_web_page_preview=True
-        )
-    else:
-        await update.message.delete()
+
 
 async def OpenSettings(event: Message, user_id: int):
     try:
