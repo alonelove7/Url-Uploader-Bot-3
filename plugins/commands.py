d4ba8506cca7507c9932876b33aa20fa3867023e
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 13:55:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,7 +11,7 @@ from pyrogram import Client, filters
 from asyncio import TimeoutError
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
-from plugins.settings import OpenSettings
+from plugins.callbacks import *
 from plugins.config import Config
 from plugins.main import Translation
 from pyrogram import Client, filters
